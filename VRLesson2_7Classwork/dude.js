c3254c3bd890dea22ab8a92ff46e873756a2b49f
jessicar400 <jessicar400@nycstudents.net>
2025-11-19 15:57:45 +0000

    update

--- VRLesson2_7Classwork/dude.js
+++ VRLesson2_7Classwork/dude.js
@@ -3,6 +3,8 @@ class Dude{
     this.x = x;
     this.y = y;
     this.z = z;
+    this.deltaY = 0.05;
+    this.deltaZ = 0.02;
     //Challenge 1: Add delta variables in order to make the dude walk forward on the z axis and jump up and down on the y axis.
 
     this.obj = dudeTemplate.cloneNode(true);
@@ -11,9 +13,37 @@ class Dude{
   }
   jump(){
     //Challenge 2: Add the accumulators to modify the y and z variables
+    this.y += this.deltaY;
+    this.z += this.deltaZ;
 
     //Challenge 3: Add the decision in order to alternate the delta on the y axis when the dude reaches less then 1 or greater than 2
+    if(this.y < 1 || this.y > 2){
+      this.deltaY = -this.deltaY;
+    }
+  }
+  update(){
+    this.jump();
 
     this.obj.setAttribute("position",{x:this.x,y:this.y,z:this.z});
   }
 }
+
+let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
+let scene, dudeTemplate, dudes = [];
+
+window.addEventListener("DOMContentLoaded",function() {
+  scene = document.querySelector("a-scene");
+  dudeTemplate = document.querySelector("#dudeTemplate");
+});
+
+function loop(){
+  //Challenge 6: Update all the dudes in the array.
+  dudes.forEach(dude => {
+    dude.update();
+  });
+  
+
+  window.requestAnimationFrame( loop );
+}
+
+  
--- VRLesson2_7Classwork/script.js
+++ VRLesson2_7Classwork/script.js
@@ -6,11 +6,24 @@ window.addEventListener("DOMContentLoaded",function() {
   dudeTemplate = document.querySelector("#dudeTemplate");
 
   //Challenge 4: Create an array of dudes at random locations.
+  for(let i=0; i<10; i++){
+    let newDude = dudeTemplate.content.cloneNode(true);
+    let x = rnd(-10,10);
+    let z = rnd(-10,10);
+    newDude.querySelector(".dude").setAttribute("position", `${x} 0 ${z}`);
+    scene.appendChild(newDude);
+  }
   
   loop();  
 })
 function loop(){
   //Challenge 5: Make all the dudes jump.
+  let dudes = document.querySelectorAll(".dude");
+  dudes.forEach(dude => {
+    let pos = dude.getAttribute("position");
+    let y = Math.abs(Math.sin(Date.now() * 0.005 + pos.x + pos.z)) * 2;
+    dude.setAttribute("position", `${pos.x} ${y} ${pos.z}`);
+  });
   
   window.requestAnimationFrame( loop );
 }
